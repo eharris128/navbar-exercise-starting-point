0e1041335c292f55831b8a423b7db2e3c58c856f
zwillerzoog <marsh.heidi.c@gmail.com>
2017-08-14 16:30:46 -0400

    added responsive, hamburger nav is next

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 "use strict";
+$(document).ready(function() {
 $("#courses").on("click", function(event) {
+  // if ($("courses").hasClass("hidden")) {
+  //   $("body").addClass("border");
+  // } 
   $(".courses").toggleClass("hidden")
   $(".info").addClass("hidden");
+  
 });
 
 $("#info").on("click", function(event) {
   $(".info").toggleClass("hidden")
   $(".courses").addClass("hidden");
+  // if ($("body").hasClass("border")) {
+  //   // $("body").removeClass("border");
+  // } 
+});
+
+
+  if ($(window).width() <= 576) {
+    $("#nav-bar").addClass("hidden");
+  } else {
+    $("#nav-bar").removeClass("hidden")
+  }
+
+
+$(".hamburger").on('click', function(event) {
+  console.log('hey')
+  $("#nav-bar").removeClass("hidden");
+})
 });
